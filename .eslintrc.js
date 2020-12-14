3572770f98e5c12ab1f0b61a11e1fc91416847c7
Guillermo GG <guillermo.gg.dev@gmail.com>
2020-12-14 09:03:46 +0100

    chore: setup repo

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: "airbnb-typescript-prettier",
+  rules: {
+    "import/prefer-default-export": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/destructuring-assignment": "off",
+    "jsx-a11y/anchor-is-valid": "off",
+    "global-require": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react/require-default-props": "off",
+    "consistent-return": "off",
+  },
+};
