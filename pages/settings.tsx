6a9f65505ec2dc3578f544d541467116a0e2aa9c
Guillermo GG <guillermo.gg.dev@gmail.com>
2020-12-28 22:17:59 +0100

    feat: finish UI for Settings

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -6,15 +6,20 @@ import { TextInput } from "components/TextInput";
 import { authContext } from "context/auth/auth.context";
 import React, { FunctionComponent, useContext } from "react";
 import styled from "styled-components";
+import { COLORS, TEXT } from "styles/constants";
 
 const SettingsContainer = styled.div`
+  max-width: 800px;
+`;
+
+const TwoColumnsContainer = styled.div`
   display: flex;
   align-items: center;
 `;
 
 const InfoContainer = styled.div`
   margin-left: 150px;
-  width: 350px;
+  flex: 1;
 `;
 
 const InfoFieldContainer = styled.div`
@@ -25,6 +30,15 @@ const InfoFieldContainer = styled.div`
 const ButtonContainer = styled.div`
   width: 100%;
   margin-top: 25px;
+`;
+
+const DisclaimerText = styled.p`
+  margin: 75px auto;
+  width: 70%;
+  text-align: center;
+
+  ${TEXT.labelSmall};
+  color: ${COLORS.text.textGray1};
 `;
 
 type SettingsProps = {};
@@ -40,20 +54,27 @@ const Settings: FunctionComponent<SettingsProps> = (props) => {
       }}
     >
       <SettingsContainer>
-        <ProfilePicture src={user?.photoURL} size="250px" />
-        <InfoContainer>
-          <InfoFieldContainer>
-            <TextInput value={user?.displayName} label="Name" />
-          </InfoFieldContainer>
-          <InfoFieldContainer>
-            <TextInput value={user?.email} label="Email" />
-          </InfoFieldContainer>
-          <ButtonContainer>
-            <Button isFilled fullWidth onClick={() => alert("Handle delete")}>
-              Delete account
-            </Button>
-          </ButtonContainer>
-        </InfoContainer>
+        <TwoColumnsContainer>
+          <ProfilePicture src={user?.photoURL} size="250px" />
+          <InfoContainer>
+            <InfoFieldContainer>
+              <TextInput value={user?.displayName} label="Name" />
+            </InfoFieldContainer>
+            <InfoFieldContainer>
+              <TextInput value={user?.email} label="Email" />
+            </InfoFieldContainer>
+            <ButtonContainer>
+              <Button isFilled fullWidth onClick={() => alert("Handle delete")}>
+                Delete account
+              </Button>
+            </ButtonContainer>
+          </InfoContainer>
+        </TwoColumnsContainer>
+        <DisclaimerText>
+          We fetch this information directly from your Google account. If you
+          want to change it, change it there, and we’ll automatically uptate it
+          here.
+        </DisclaimerText>
       </SettingsContainer>
     </PrivateContainer.Content>
   );
